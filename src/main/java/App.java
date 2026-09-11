22a4d27e3e81895441668401985c987f00b143de
ayush7237 <145737542+ayush7237@users.noreply.github.com>
2026-09-11 09:12:06 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Maven Project!");
+        System.out.println("2 + 3 = " + (2 + 3));
+    }
+}
